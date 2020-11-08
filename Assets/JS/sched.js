02b2d5415629f4a46b5efe1baa75e7916e7e5ba5
SCN <supercodingninja@outlook.com>
2020-11-08 05:35:24 -0800

    JS changes

--- Assets/JS/sched.js
+++ Assets/JS/sched.js
@@ -8,4 +8,6 @@ var sched = wd.sched;
 var Calendar = new sched.Calendar(document.getElementById("myCalendar"));
     Calendar.render();
 
-    
+// Displays Format Date and Time //
+const dateView = 'YYYY-MM-DD';
+const 
