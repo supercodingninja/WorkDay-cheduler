4c1c4d68d010a8db812ff8690c00b787677d5ad4
SCN <supercodingninja@outlook.com>
2020-11-09 20:27:15 -0800

    Create month schedule; but I could not figure how to differentiate the day and hours..original thought was the ability to go to full day when the user clicks on the day of the month calendar.

--- Assets/JS/sched.js
+++ Assets/JS/sched.js
@@ -10,4 +10,23 @@ var Calendar = new sched.Calendar(document.getElementById("myCalendar"));
 
 // Displays Format Date and Time //
 const dateView = 'YYYY-MM-DD';
-const 
+const currDate = '2020-11-07';
+const myMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'Decemeber'];
+const shortMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'Decemeber'];
+const myWeek = 7;
+const myDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const shortDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const myHours = 24;
+
+// How do I know my current date and time: //
+var currTime = moment(currDate, dateView);
+// render currTime(); //
+
+// I need to start the month //
+var startDate = currTime.startOf('month');
+
+// I need to start the week of the month //
+// ?? var weekStart = currTime.startOf('week'); ?? //
+var weekStart = startDate.week();
+
+// I NEED TO FIGURE OUT HOW TO MAKE THIS INTO INDIVIDUAL DAYS!  THIS IS SUPPOSE TO BE A DAY PLANNER SHOWING...creating separate JS file, just in case I can make this work the way I visualized.  I need a MVP, first. //
