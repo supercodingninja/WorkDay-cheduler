f7255c35f9b68a237cf3dc348b99030b8db09e88
SCN <supercodingninja@outlook.com>
2020-11-14 03:56:46 -0800

    Pseudo Code for JS + jQuery

--- Assets/JS/day.js
+++ Assets/JS/day.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// I 
+// I need to know what time it is. //
+
+...
+
+// Event Listener needed: "When Clicked, do this..." //
+
+...
+
+// I need to know what the date is. //
+
+...
+
+// I will need to figure out local storage. //
+
+...
