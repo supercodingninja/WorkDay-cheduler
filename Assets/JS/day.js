da5aec9a804b8b676eb3d7d7169221d1066c9cd0
SCN <supercodingninja@outlook.com>
2020-11-18 14:16:58 -0800

    Created functions for hours to be displayed for tasks, clock; and whether the tasks expired, are current, or upcoming (in relation to my clock time being display).

--- Assets/JS/day.js
+++ Assets/JS/day.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// Hours of of day established. //
 const t4Hours = {
     "0000": "",
     "0100": "",
@@ -28,23 +27,68 @@ const t4Hours = {
     "2300": "",
 };
 
-// I need to know what time it is. //
+function clockString(hoursString){
+    switch (hoursString){ // Switch statement https://www.w3schools.com/js/js_switch.asp //
+        case "0000": return 00;
+        case "0100": return 1;
+        case "0200": return 2;
+        case "0300": return 3;
+        case "0400": return 4;
+        case "0500": return 5;
+        case "0600": return 6;
+        case "0700": return 7;
+        case "0800": return 8;
+        case "0900": return 9;
+        case "1000": return 10;
+        case "1100": return 11;
+        case "1200": return 12;
+        case "1300": return 13;
+        case "1400": return 14;
+        case "1500": return 15;
+        case "1600": return 16;
+        case "1700": return 17;
+        case "1800": return 18;
+        case "1900": return 19;
+        case "2000": return 20;
+        case "2100": return 21;
+        case "2200": return 22;
+        case "2300": return 23;
+    }; 
+};
 
-...
-
-// Event Listener needed: "When Clicked, do this..." //
-
-...
-
-// I need to know what the date is. //
-
-...
-
-// I will need to figure out local storage. //
 $(document).ready(function(){
     if(localStorage.getItem('t4hours')){
         tasks(t4Hours);
-    }; else {
+    }
+    
+    else {
         tasks(JSON.parse(localStorage.getItem('t4Hours')))
     };
 });
+
+$('#currDate h4').text(moment().format('DDDD') + "," + moment().format('MMMM do YYYY, hh:mm'));
+
+let counter = 1;
+
+for(const property in t4Hours){
+    let taskEntered = "#taskEntered" + counter;
+    $(taskEntered).text(t4Hours[property]);
+    let clockTime = "#clock" + counter;
+    let clockHour = moment().hour();
+    let stringTime = $(clockTime).text();
+    let schedTask = clockString(hoursString);
+
+        if (schedTask < clockHour){
+            $(taskEntered).addClass("past"); 
+        };
+        
+        else if (schedTask > clockHour){
+            $(taskEntered).addClass("future"); 
+        };
+        
+        else {
+            $(taskEntered).addClass("current"); 
+        };
+      counter++;  
+};
+
